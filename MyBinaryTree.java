bfa265ab4a41ad592f35c8085aef2d0dc2250e65
unknown <narmada.vaddireddy97@gmail.com>
2023-03-11 21:35:51 +0530

    Added: Code to print add using binary search tree

---
+++ MyBinaryTree.java
@@ -0,0 +1,32 @@
+public class MyBinaryTree<K extends Comparable<K>>  {
+    private MyBinaryNode<K> root;
+    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key){
+        if(current == null) {
+            return new MyBinaryNode<K>(key);
+        }
+        int compareResult = key.compareTo(current.key);
+        if(compareResult == 0) {
+            return current;
+        }
+        if(compareResult < 0) {
+            current.left = addRecursively(current.left, key);
+        }
+        else {
+            current.right = addRecursively(current.right, key);
+        }
+        return current;
+
+    }
+    public void add(K key) {
+        this.root = this.addRecursively(root, key);
+    }
+
+    public void printBinaryTree() {
+        System.out.println("My Tree: " + root);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBinaryTree{" + root + '}';
+    }
+}
---
+++ MyBinaryTreeMain.java
@@ -0,0 +1,9 @@
+public class MyBinaryTreeMain {
+    public static void main(String[] args) {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.printBinaryTree();
+    }
+}
